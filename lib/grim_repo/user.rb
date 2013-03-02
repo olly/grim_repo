4b62869cd3ec60132bb9694dd7f720681b05bf7c
Olly Legg <olly@51degrees.net>
2013-03-02 11:10:04 +0000

    Parse additional User attributes

--- lib/grim_repo/user.rb
+++ lib/grim_repo/user.rb
@@ -1,9 +1,33 @@
 module GrimRepo
   class User
     def initialize(data)
+      @api_url = parse_uri(data['url'])
+      @avatar_url = parse_uri(data['avatar_url'])
+      @bio = data['bio']
+      @blog = parse_uri(data['blog'])
+      @company = data['company']
+      @created_at = Time.iso8601(data['created_at'])
+      @email = data['email']
+      @gravatar_id = data['gravatar_id']
+      @hireable = data['hireable']
+      @id = data['id']
+      @location = data['location']
       @login = data['login']
+      @name = data['name']
+      @url = parse_uri(data['html_url'])
     end
 
-    attr_reader :login
+    attr_reader :api_url, :avatar_url, :bio, :blog, :company, :created_at,
+                :email, :gravatar_id, :id, :location, :login, :name, :url
+
+    def hireable?
+      @hireable
+    end
+
+    private
+    def parse_uri(uri)
+      return if uri.nil?
+      URI.parse(uri)
+    end
   end
 end
---
+++ spec/grim_repo/user_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'json'
+require 'grim_repo/user'
+
+describe GrimRepo::User do
+  describe "#initialize" do
+    subject { GrimRepo::User.new(fixture('user.json')) }
+
+    it { should_not be_hireable }
+    its(:api_url) { should == URI.parse("https://api.github.com/users/octocat") }
+    its(:avatar_url) { should == URI.parse('https://github.com/images/error/octocat_happy.gif') }
+    its(:bio) { should == 'There once was...' }
+    its(:blog) { should == URI.parse('https://github.com/blog') }
+    its(:company) { should == 'GitHub'}
+    its(:created_at) { should == Time.utc(2008, 1, 14, 4, 33, 35) }
+    its(:email) { should == 'octocat@github.com' }
+    its(:gravatar_id) { should == 'somehexcode' }
+    its(:id) { should == 1 }
+    its(:location) { should == 'San Francisco'}
+    its(:login) { should == 'octocat' }
+    its(:name) { should == 'monalisa octocat' }
+    its(:url) { should == URI.parse("https://github.com/octocat") }
+  end
+
+  def fixture(filename)
+    file = Pathname.new(__dir__).join('..', 'fixtures', 'data', filename)
+    JSON.parse(file.read)
+  end
+end
