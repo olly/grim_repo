72663427c3de9ce17b1157208d3533943357d9af
Olly Legg <olly@51degrees.net>
2013-03-04 18:43:32 +0000

    Add Repository#languages

--- lib/grim_repo/repository.rb
+++ lib/grim_repo/repository.rb
@@ -1,3 +1,6 @@
+require 'grim_repo/language'
+require 'grim_repo/uris'
+
 module GrimRepo
   class Repository
     # Creates a new repository with data from the API.
@@ -8,10 +11,23 @@ module GrimRepo
       @client = client
 
       @name = data['name']
+      @full_name = data['full_name']
     end
 
     # @return [String]
     attr_reader :name
+
+    # @return [String]
+    attr_reader :full_name
+
+    # The programming languages used in the repository.
+    #
+    # @return [Array<Language>]
+    def languages
+      client.get(URIs::RepositoryLanguages[self.full_name]).map do |name, bytes|
+        Language.new(name, bytes)
+      end
+    end
 
     private
 
--- lib/grim_repo/uris.rb
+++ lib/grim_repo/uris.rb
@@ -2,6 +2,7 @@ module GrimRepo
   # @api private
   module URIs
     UserRepositories = ->(login) { URI.parse("https://api.github.com/users/#{login}/repos") }
+    RepositoryLanguages = ->(full_repository_name) { URI.parse("https://api.github.com/#{full_repository_name}/languages") }
 
     AuthenticatedUser = URI.parse('https://api.github.com/user')
     User = ->(login) { URI.parse("https://api.github.com/users/#{login}") }
