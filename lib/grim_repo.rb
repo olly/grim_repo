f1f5c5cba2bcaa0679cfc511c5668f0cd2e0606f
Olly Legg <olly@51degrees.net>
2013-03-04 18:34:32 +0000

    Add Language model

--- lib/grim_repo.rb
+++ lib/grim_repo.rb
@@ -2,6 +2,7 @@ require "grim_repo/version"
 
 module GrimRepo
   autoload :Client, 'grim_repo/client'
+  autoload :Language, 'grim_repo/language'
   autoload :Repository, 'grim_repo/repository'
   autoload :Repositories, 'grim_repo/repositories'
   autoload :User,   'grim_repo/user'
